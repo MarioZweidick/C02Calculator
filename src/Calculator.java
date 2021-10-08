2cc6cf4e71500a76f49047275502657f04e384f0
MarioZweidick <mario.zweidick2@gmail.com>
2021-10-08 19:59:19 +0200

    Added some variables of different data types and created output for console

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,19 @@ public class Calculator
 {
     public static void main(String[] args)
     {
+        int nInteger = 2;
+        double dDouble = 3.3;
+        boolean bBool = true;
+        char cChar = 'T';
+        String strText = "TEST";
+
+        System.out.println(nInteger);
+        System.out.println(dDouble);
+        System.out.println(bBool);
+        System.out.println(cChar);
+        System.out.println(strText);
+
+
 
     }
 }
