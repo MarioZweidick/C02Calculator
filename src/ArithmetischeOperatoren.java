ff8244f5b4c9f5ebc92076d1f35a205f73d7321f
MarioZweidick <mario.zweidick2@gmail.com>
2021-10-08 20:23:17 +0200

    Added some variables of different data types and created output for console

---
+++ src/ArithmetischeOperatoren.java
@@ -0,0 +1,25 @@
+public class ArithmetischeOperatoren
+{
+
+
+    public static void main(String[] args)
+    {
+        int i1 = 1;
+        int i2 = 3;
+
+        //addition
+        System.out.println(i1+i2);
+
+        //subtraktion
+        System.out.println(i1-i2);
+
+        //multiplikation
+        System.out.println(i1*i2);
+
+        //Division
+        System.out.println(i2/i1);
+
+        //Modulo
+        System.out.println(i1%i2);
+    }
+}
